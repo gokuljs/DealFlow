f65add4581eb1dd38625960917b01856223e58df
gokuljs <gokuljs029@gmail.com>
2024-02-03 23:55:53 +0530

    tailwind config file

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -5,7 +5,7 @@ export interface NavBarProps {
     heading: string;
 }
 
-const Navbar: React.FC<NavBarProps> = ({ heading }): JSX.Element => {
+const Navbar = ({ heading }: NavBarProps): JSX.Element => {
     return (
         <div className='w-full h-[88px] py-5 px-10 flex justify-between items-center'>
             <h1 className='text-[27px] font-bold'>{heading}</h1>
