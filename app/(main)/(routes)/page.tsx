7a8293793d3aba5d4e294aa7ec1d6e971faa7a3c
gokuljs <gokuljs029@gmail.com>
2024-02-04 03:34:12 +0530

    design studio pages

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const page = (): JSX.Element => {
-    return <div>Home page</div>;
+    return (
+        <div className='h-full w-full flex justify-between mt-4'>
+            <div className='flex-1 flex items-center justify-center pr-4'>
+                <div className='h-full w-full bg-white rounded-[20px]'></div>
+            </div>
+            <div className='flex-1 flex items-center justify-center pl-4'>
+                <div className='h-full w-full bg-white rounded-[20px]'></div>
+            </div>
+        </div>
+    );
 };
 
 export default page;
